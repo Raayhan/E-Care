3ad01592a2ad6f124514e1e09ea31b1c9fdc288b
Raayhan <rayhan.rakib@northsouth.edu>
2020-12-30 16:22:16 +0600

    Updates on Admin Profile controller

--- app/Http/Controllers/Admin/Profile/ViewProfileController.php
+++ app/Http/Controllers/Admin/Profile/ViewProfileController.php
@@ -10,36 +10,37 @@ use Hash;
 
 class ViewProfileController extends Controller
 {
-    public function ViewProfile(){
+    public function ViewProfile(){ // View Admin Settings page
  
         return view('admin.profile.view');
        
     }
-    public function ViewPassword(){
+    public function ViewPassword(){ // View admin password change page
         
         return view('admin.profile.password');
        
     }
 
-    public function ChangeName(Request $request){
+    public function ChangeName(Request $request){ // Change admin Email and  Name function
         
-        $this->Validator($request);        
-        $id = $request->input('id');
-        $password = $request->input('password');
+        $this->Validator($request);    // Validator function    
+        $id = $request->input('id'); // get the admin id
+        $password = $request->input('password'); // Get the admin password
  
 
-        $admin = Admin::findOrFail($id);
+        $admin = Admin::findOrFail($id); // Checking the id
 
-        if (Hash::check($password, $admin->password)) {
+        if (Hash::check($password, $admin->password)) {  // If given password matches
         
-            $admin->name = $request->input('name');
+            $admin->name = $request->input('name'); // Updating Admin Email and Name
             $admin->email= $request->input('email');
-            $admin->save();
-            return redirect()->to('/admin/profile/view')->with('status','Changes Saved');
+            $admin->save(); // Saving to the database
+            return redirect()->to('/admin/profile/view')->with('status','Changes Saved'); // Redirecting to the previous page with session message
 
         }
-        else {
+        else { // If password does not matches 
             return redirect()->to('/admin/profile/view')->with('error','Incorrect Password');
+            // Redirecting to the previous page with session error message
         }
        
    
@@ -59,38 +60,36 @@ class ViewProfileController extends Controller
             ];
 
             //custom validation error messages.
-            $messages = [
-               
+            $messages = [   
                
             ];
-            
 
             //validate the request.
             $request->validate($rules,$messages);
 
-
     }
 
 
-    public function ChangePassword(Request $request){
+    public function ChangePassword(Request $request){ // Change password function
         
-        $this->PasswordValidator($request);        
-        $id = $request->input('id');
-        $oldpassword = $request->input('oldpassword');
-        $newpassword = $request->input('password');
-        $hash = Hash::make($newpassword);
+        $this->PasswordValidator($request);   //Validate the form     
+        $id = $request->input('id'); // Get the admin id
+        $oldpassword = $request->input('oldpassword'); // Get old password
+        $newpassword = $request->input('password'); // Get new password
+        $hash = Hash::make($newpassword); // Making new password hashed
 
-        $admin = Admin::findOrFail($id);
+        $admin = Admin::findOrFail($id); // checking for the admin id to the database
 
-        if (Hash::check($oldpassword, $admin->password)) {
+        if (Hash::check($oldpassword, $admin->password)) { // If old password matches
         
-            $admin->password = $hash;
-            $admin->save();
+            $admin->password = $hash; // Inserting new password
+            $admin->save(); // Saving into the database
             return redirect()->to('/admin/profile/password')->with('status','Password Changed');
-
+           // Redirecting to the previous page with session message
         }
-        else {
+        else { // If old password does not matches
             return redirect()->to('/admin/profile/password')->with('error','Incorrect Password');
+            // Redirecting to the previous page with session message
         }
        
    
@@ -104,8 +103,6 @@ class ViewProfileController extends Controller
                 
                 'password'     => 'required|min:6|max:255|confirmed',
                 'oldpassword'  => 'required|min:6|max:255',
-               
-                
                
             ];
 
